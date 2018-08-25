45ae4423f7933e0e159a32797dfb8f137d182832
Alessandro Oliveira <alessandro.fillus@gmail.com>
2018-08-24 22:20:31 -0300

    initial project

---
+++ XamarinStarWars/XamarinStarWars/Services/StarWarsService.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinStarWars.Services
+{
+    public class StarWarsService
+    {
+        readonly string _BASE_URL = "https://swapi.co/api";
+
+        public async Task<List<string>> GetAllPeopleAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                var json = await client.GetStringAsync($"{_BASE_URL}/people");
+                return JsonConvert.DeserializeObject<List<string>>(json);
+            }
+        }
+    }
+}
---
+++ XamarinStarWars/XamarinStarWars/Views/BaseViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XamarinStarWars.Services;
+
+namespace XamarinStarWars.Views
+{
+   public abstract class BaseViewModel : BindableObject
+    {
+        public StarWarsService Api => new StarWarsService();
+        public INavigation Navigation => Application.Current.MainPage.Navigation;
+
+        public ICommand RefreshCommand { get; set; }
+        public ICommand ItemClickCommand { get; set; }
+
+        bool isBusy = false;
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { isBusy = value; OnPropertyChanged(); }
+        }
+
+        string title = string.Empty;
+        public string Title
+        {
+            get { return title; }
+            set { title = value; OnPropertyChanged(); }
+        }
+
+        public BaseViewModel(string title)
+        {
+            Title = title;
+        }
+        
+        public async Task ShowAlertAsync(string title, string msg, string cancel)
+        {
+            await Application.Current.MainPage.DisplayAlert(title, msg, cancel);
+        }
+
+        public virtual Task Initialize(object parameters = null) => Task.FromResult(true);
+    }   
+}
